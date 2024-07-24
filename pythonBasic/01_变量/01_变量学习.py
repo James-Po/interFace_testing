7ca1988609e9131a2975ad7ca9a984639506cf56
itjiangpo <itjiangpo@163.com>
2024-07-24 19:48:47 +0800

    变量学习

--- pythonBasic/01_变量/01_变量学习.py
+++ pythonBasic/01_变量/01_变量学习.py
@@ -2,4 +2,13 @@
 name = '杨江坡'
 print(name)
 
-print('name')
+# 定义一个字段，保存你的年龄
+
+age = 18
+print(age)
+
+# 打印下自己的名字，年龄
+print("My name is" + name + ",我 " + str(age) + " 岁了")
+
+print(type(age))
+print(type(str(age)))
