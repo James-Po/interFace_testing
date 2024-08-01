f36de6f87c0b05174a655f41c55c3cf6aaa423f8
itjiangpo <itjiangpo@163.com>
2024-08-01 19:51:59 +0800

    容器学习-字符串

--- 1_pythonBasic/04_容器/01_字符串.py
+++ 1_pythonBasic/04_容器/01_字符串.py
@@ -7,3 +7,25 @@ str3 = """hello"""
 str4 = '''hello'''
 
 print(type(str1), type(str2), type(str3), type(str4))
+
+# 4. 定义字符串  I'm 小明, 字符串本身包含引号
+# 4.1 如果字符串本身包含单引号,定义的时候不能使用 单引号,
+# 4.2 如果字符串本身包含双引号,定义的时候不能使用 双引号,
+
+str5 = "I'm 小明"
+print(str5)  # I'm 小明
+
+# 5. 转义字符 \n \t \' \"
+str6 = 'I\'m 小明'
+print(str6)  # I'm 小明
+
+# 6. I\'m 小明  \\ --> \
+str7 = 'I\\\'m 小明'
+print(str7)  # I\'m 小明
+
+# 7. 原生字符串 在字符串的前边 加上 r"", 字符串中的 \ 就不会进行转义
+str8 = r'I\'m 小明'
+print(str8)  # I\'m 小明
+
+str7 = r'I\\\'m 小明'
+print(str7)  # I\\\'m 小明
