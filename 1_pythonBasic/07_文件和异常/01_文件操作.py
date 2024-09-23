43d9e87960077f55f4b5245e922f89b11d43c7df
itjiangpo <itjiangpo@163.com>
2024-09-23 16:43:45 +0800

    文件操作

--- 1_pythonBasic/07_文件和异常/01_文件操作.py
+++ 1_pythonBasic/07_文件和异常/01_文件操作.py
@@ -1,3 +1,19 @@
 with open("a.txt", "r", encoding="utf-8") as f:
     for i in f:
         print(i, end='')
+
+    f.close()
+
+with open("a.txt", "r", encoding="utf-8") as f:
+    result = f.read()
+    print(result)
+    f.close()
+
+with open("a.txt", "w", encoding="utf-8") as f:
+    f.write("hello world")
+    f.close()
+
+with open("a.txt", "r", encoding="utf-8") as f:
+    result1 = f.read()
+    print(result1)
+    f.close()
