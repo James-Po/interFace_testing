e96ea7a73018a48f3c3a45394988cc3e3d21997e
itjiangpo <itjiangpo@163.com>
2024-12-20 13:55:27 +0800

    testcase

--- 04_FrameWork/01_unitest框架/01_test.py
+++ 04_FrameWork/01_unitest框架/01_test.py
@@ -1,8 +1,9 @@
 import unittest
 
-class Test(unittest.TestCase):
-    def test_1(self):
+class TestDemo(unittest.TestCase):
+    def test_method1(self):
         self.assertEqual(1, 1)
         self.assertEqual(2, 2)
 
-    
+if __name__ == '__main__':
+    unittest.main()
