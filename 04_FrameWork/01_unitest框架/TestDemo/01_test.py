40032c659e381d9eaa0c6a05188f969cb28260de
itjiangpo <itjiangpo@gmail.com>
2025-02-13 20:43:01 +0800

    框架学习

--- 04_FrameWork/01_unitest框架/TestDemo/01_test.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-class TestDemo(unittest.TestCase):
-    def test_method1(self):
-        self.assertEqual(1, 1)
-        self.assertEqual(2, 2)
-
-if __name__ == '__main__':
-    unittest.main()
---
+++ 04_FrameWork/01_unitest框架/TestDemo/yjp_01_testcase.py
@@ -0,0 +1,25 @@
+# 1、导包
+import unittest
+
+# 2、定义测试类，只要继承unittest.TestCase 类，就是 测试类
+class TestDemo(unittest.TestCase):
+    # 3、书写测试方法，方法中的代码就是真正用例代码，方法名称必须以 test 开头
+    def test_method1(self):
+        self.assertEqual(1, 1)
+        self.assertEqual(2, 2)
+        print('这是个测试方法')
+
+    def test_method2(self):
+        print('这是个测试方法2')
+
+
+'''
+4、执行
+4.1 再类名或者方法名后边右键运行
+4.1.1 在类名后面，执行类中的所有的测试方法
+4.1.2 在方法名后边，只执行当前的测试方法
+'''
+
+# 4.1 在住程序使用 unittest.main() 来执行
+if __name__ == '__main__':
+    unittest.main()
